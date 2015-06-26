efddcaf07890c98a589d0c152aa4008d58d9ce2b
rageandqq <chitley.sameer@gmail.com>
2015-06-26 19:31:24 -0400

    Project Initialized.

---
+++ myo.js
@@ -0,0 +1,8 @@
+ var Myo = require('myo');
+
+ var myMyo = Myo.create();
+ myMyo.on('fist', function(edge){
+       if(!edge) return;
+           console.log('Hello Myo!');
+               myMyo.vibrate();
+ });
