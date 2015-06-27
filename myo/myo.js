965f74cfd4fed699b9bd5fb1c0e1792d11b0384f
rageandqq <chitley.sameer@gmail.com>
2015-06-27 04:36:25 -0400

    Added all handlers and communication between Myo and Server

--- myo/myo.js
+++ myo/myo.js
@@ -7,24 +7,23 @@ var MyoReader = module.exports = function(ServerHelper, debug) {
   var SAMPLE_PERIOD = 20;
   var Z_THRESHOLD = 0.1;
   var HEIGHT_THRESHOLD = 0.1;
+  var ROTATION_THRESHOLD = 0.1;
   var HEIGHT_SAMPLE_SIZE = 4;
+  var ROTATION_SAMPLE_SIZE = 4;
   var self = this;
 
   //instance variables
-  this.delta = 0;
-  this.locked = true;
-  this.zVal = 0;
-  this.awaitingPosition = false;
-  this.rotationValue = 0;
-  this.heightValue = 0;
-  this.rotationRest = true;
-  this.heightRest = true;
-  this.devices = [];
   this.debug = debug || true;
+  this.delta = this.zVal = this.rotationValue = this.heightValue = 0;
+  this.rotationState = this.heightState = 'none';
+
+  this.locked = true;
+  this.awaitingPosition = false;
+
+  this.devices = [];
   this.heightSamples = new BoundedArray(HEIGHT_SAMPLE_SIZE);
-  this.heightState = 'none';
+  this.rotationSamples = new BoundedArray(ROTATION_SAMPLE_SIZE);
   this.currentDevice = null;
-  this.currentDeviceName = 'none';
 
   //Create myo connection
   var myo = Myo.create();
@@ -39,7 +38,7 @@ var MyoReader = module.exports = function(ServerHelper, debug) {
       if (this.locked) {
         console.log('locking Myo\n');
       } else {
-        myo.zeroOrientation(); //current orientation is now considered 'home'
+        myo.zeroOrientation(); //current orientation is now considered (0,0,0)
         console.log('unlocking Myo\n');
       }
       myo.vibrate();
@@ -47,9 +46,7 @@ var MyoReader = module.exports = function(ServerHelper, debug) {
   }.bind(this));
 
   myo.on('fingers_spread', function(edge) {
-    console.log('detected spread');
     if (edge && !this.locked && this.awaitingPosition) {
-      console.log('added device at pos: ' + this.zVal);
       this.awaitingPosition = false;
       ServerHelper.addDevice(this.zVal);
     }
@@ -58,77 +55,98 @@ var MyoReader = module.exports = function(ServerHelper, debug) {
   myo.on('imu', function(data) {
     this.delta++;
     if (this.delta >= SAMPLE_PERIOD){
+      if (this.devices.length > 0)
+        analyze(this.devices);
       this.delta %= SAMPLE_PERIOD;
       this.zVal = data.orientation.z; //set most recent zValue
+
       if (!this.locked) {
+
+        if (this.debug) { //debug
+          printDevices(this.devices);
+          console.log('current device: ' + (this.currentDevice != null ? this.currentDevice.socket.id:'none'));
+        }
+
         if (this.devices.length > 0) {
-          analyze(this.devices);
+          //TODO: Fix overly sensitive rotation and height
+          //handleRotation(data.accelerometer.y);
+          //handleHeight(data.accelerometer.x);
         }
-        if (this.debug) {
-          printDevices(this.devices); //print device list
-        }
-        //handleRotation(data.accelerometer.y); //TODO: IMPLEMENT
-        handleHeight(data.accelerometer.x);
-        console.log('current device: ' + this.currentDeviceName);
       }
     }
   }.bind(this));
 
-  function handleHeight(val) {
-    self.heightSamples.push(val);
-    var state = analyzeTrend(self.heightSamples.getArray());
-    if (state == 'none' && state != self.heightState) {
-      if (self.heightState == 'increasing') {
-        //TODO: Use Callback from Server
-        console.log('increase volume')
+  function handleRotation(val) {
+    self.rotationSamples.push(val);
+    var state = analyzeTrend(self.rotationSamples.getArray(), ROTATION_THRESHOLD);
+    if (state == 'none' && state != self.rotationState) {
+      if (self.rotationState == 'increasing') {
+        console.log('increase tempo')
+        ServerHelper.increaseTempo(self.currentDevice.socket);
       }
       else {
-        //TODO: Use Callback from Server
-        console.log('decrease volume')
+        console.log('decrease tempo')
+        ServerHelper.decreaseTempo(self.currentDevice.socket);
       }
-      self.locked = true;
+
+      self.locked = true; //lock temporarily
       setTimeout(function() {
         self.locked = false;
       }, 2000);
+      self.rotationSamples.clear();
+    }
+    self.rotationState = state;
+  }
 
+  function handleHeight(val) {
+    self.heightSamples.push(val);
+    var state = analyzeTrend(self.heightSamples.getArray(), HEIGHT_THRESHOLD);
+    if (state == 'none' && state != self.heightState) {
+      if (self.heightState == 'increasing') {
+        console.log('increase volume')
+        ServerHelper.increaseVolume(self.currentDevice.socket);
+      }
+      else {
+        console.log('decrease volume')
+        ServerHelper.decreaseVolume(self.currentDevice.socket);
+      }
+
+      self.locked = true; //lock temporarily
+      setTimeout(function() {
+        self.locked = false;
+      }, 2000);
       self.heightSamples.clear();
     }
     self.heightState = state;
   }
 
-  function analyzeTrend(arr) {
-    if (isIncreasing(arr))
+  function analyzeTrend(arr, thresh) {
+    if (isIncreasing(arr, thresh))
       return 'increasing';
-    if (isDecreasing(arr))
+    if (isDecreasing(arr, thresh))
       return 'decreasing';
     return 'none';
   }
 
-  function isIncreasing(arr) {
+  function isIncreasing(arr, thresh) {
     var dist = 0;
     for (var i = 1; i < arr.length; i++) {
       dist += (arr[i] - arr[i-1]);
     }
     dist/= arr.length;
-    return dist - HEIGHT_THRESHOLD > 0;
+    return dist - thresh > 0;
   }
 
-  function isDecreasing(arr) {
+  function isDecreasing(arr, thresh) {
     var dist = 0;
     for (var i = 1; i < arr.length; i++) {
       dist += (arr[i] - arr[i-1]);
     }
     dist /= arr.length;
-    return dist + HEIGHT_THRESHOLD < 0;
+    return dist + thresh < 0;
   }
 
-  function handleRotation(val) {
-    if (self.debug) {
-      console.log('Rotation: ' + val);
-    }
-  }
-
-  //print device list (unused)
+  //print device list
   function printDevices (deviceList) {
     if (deviceList.length > 0) {
       console.log('Device list: ');
@@ -141,12 +159,11 @@ var MyoReader = module.exports = function(ServerHelper, debug) {
     }
   }
 
-  //analyze orientation for devices and update current device (currently unused)
+  //analyze orientation for devices and update current device
   function analyze(deviceList) {
     for (var i in deviceList) {
       var d = deviceList[i];
       if (d.z >= self.zVal - Z_THRESHOLD && d.z <= self.zVal + Z_THRESHOLD) {
-        self.currentDeviceName = d.socket.id; //update current device pointed to
         self.currentDevice = d;
       }
     }
